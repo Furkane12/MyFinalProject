13c88f41151ae774c9fe97b318aae2fb72d9394e
Furkan <Furkan@DESKTOP-55EO9CF>
2025-09-10 23:15:22 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+	//IEntity nin yanında bulunan "Product" bir veritabanı tablosudur. Anlamamız gerekiyor, IEntity referans olarak "Product" tutuyor.
+	public class Product: IEntity
+	{
+		public int ProductID { get; set; }
+		public int CategoryId { get; set; }
+		public string ProductName { get; set; }       
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice  { get; set; }
+    }
+}
